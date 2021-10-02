0380fb50b1b5a05f6416a4a0c721b55393166b8f
Mihaela Marson <mitchmarson666@gmail.com>
2021-10-02 23:02:31 +0200

    adding file

--- form/validation.php
+++ form/validation.php
@@ -5,15 +5,30 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
+  <style>
+    .error{
+      color: red;
+    }
+  </style>
 </head>
 <body>
 <?php
   $name = $email = $password = "";
 
   if($_SERVER["REQUEST_METHOD"] == "POST"){ // if method is post - if data vas posted
-    $name = val($_POST["fname"]);
-    $email = val($_POST["email"]);
-    $password = val($_POST["password"]);
+    if(empty($_POST["email"])){
+      echo '<span class="error">Email Required</span>';
+    }elseif(!filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)){
+      echo '<span class="error">Email not in valid format</span>';
+    }
+    elseif(empty($_POST["password"])){
+      echo '<span class="error">Password Required</span>';
+    }else{
+      $name = val($_POST["fname"]);
+      $email = val($_POST["email"]);
+      $password = val($_POST["password"]);
+    }
+    
   }
 
   function val($data){
